3b97c45a3e4d39f2203e191afcc426bd1439491d
B3rzxd3v <d3vilc0re@gmail.com>
2023-05-10 00:44:02 +0700

    Add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
+
+mongoose.connect(process.env.DATABASE, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => console.log('DB connection successful'));
+
 const app = require('./app');
 
 app.listen(process.env.PORT, () => {
