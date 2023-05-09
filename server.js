c309a9f6f302c9a4e449c818679bf36b98f50003
B3rzxd3v <d3vilc0re@gmail.com>
2023-05-10 01:17:52 +0700

    Add Tour model for mongoose

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true
 }).then(() => console.log('DB connection successful'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'Tour must have a name']
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a price']
+  },
+  rating: {
+    type: Number,
+    default: 4
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
 const app = require('./app');
 
 app.listen(process.env.PORT, () => {
